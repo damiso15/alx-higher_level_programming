0366c6e8f632aaa11fc95db769f3ee426289e412
damiso <damiso15@yahoo.com>
2023-03-09 07:25:54 -0800

    1. My first toolbox!

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,15 +6,10 @@ def main():
     a = 10
     b = 5
 
-    addition = add(a, b)
-    minus = sub(a, b)
-    multiplication = mul(a, b)
-    division = div(a, b)
-
-    print("{:d} + {:d} = {:d}".format(a, b, addition))
-    print("{:d} - {:d} = {:d}".format(a, b, minus))
-    print("{:d} x {:d} = {:d}".format(a, b, multiplication))
-    print("{:d} / {:d} = {:d}".format(a, b, division))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} x {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
 
 
 if __name__ == "__main__":
