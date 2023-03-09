f758da359fc07f3a6b2dcf9f4dcc70c9c90ed246
damiso <damiso15@yahoo.com>
2023-03-09 07:50:50 -0800

    1. My first toolbox!

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -9,7 +9,8 @@ def main():
     operator = {0: add, 1: sub, 2: mul, 3: div}
 
     for sign in operator:
-        print('{:d} {:} {:d} = {:d}'.format(a, symbols[sign], b, operator[sign](a, b)))
+        print("{:d} {:} {:d} = {:d}".format(a, symbols[sign], b,
+                                            operator[sign](a, b)))
 
 
 if __name__ == "__main__":
