819658d31a7fd3ff1f830e692dc9dbf322cb2449
damiso <damiso15@yahoo.com>
2023-03-28 14:10:33 -0700

    10. ByteCode -> Python #5

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+import math
+
+"""
+A class MagicClass
+"""
+
+
+class MagicClass:
+    def __init__(self, radius=0):
+        """ Instantiation with radius """
+
+        self.__radius = 0
+        self.radius = radius
+
+    @property
+    def radius(self):
+        """ The property for the radius method to retrieve it """
+
+        return self.__radius
+
+    @radius.setter
+    def radius(self, value):
+        """ The property for the radius method to set it """
+
+        if type(value) not in [int, float]:
+            raise TypeError('radius must be a number')
+        elif value < 0:
+            raise ValueError('radius must be >= 0')
+        else:
+            self.__radius = value
+
+    def area(self):
+        """ A public instance method that returns the current square area """
+
+        return self.__radius ** 2 * math.pi
+
+    def circumference(self):
+        """ A public instance method that returns the current circumference """
+
+        return 2 * math.pi * self.__radius
