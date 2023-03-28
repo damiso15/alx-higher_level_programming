4ecc79ac6b62d40a7349b9384fe02a3a334424c8
damiso <damiso15@yahoo.com>
2023-03-28 08:59:10 -0700

    5. Printing a square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -31,7 +31,8 @@ class Square:
         return self.__size ** 2
 
     def my_print(self):
-        """A public instance method that prints in stdout the square with the character #"""
+        """A public instance method that prints in stdout the square with
+        the character #"""
         if self.__size == 0:
             print()
         else:
