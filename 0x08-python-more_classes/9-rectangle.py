65cae2b4e851bfc8c85e81e5ca901cafde74c465
damiso <damiso15@yahoo.com>
2023-04-04 02:51:17 -0700

    9. A square is a rectangle

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -57,7 +57,7 @@ class Rectangle:
             raise TypeError("height must be an integer")
 
         if value < 0:
-            raise TypeError("height must be >= 0")
+            raise ValueError("height must be >= 0")
 
         self.__height = value
 
@@ -69,7 +69,7 @@ class Rectangle:
             raise TypeError("width must be an integer")
 
         if value < 0:
-            raise TypeError("width must be >= 0")
+            raise ValueError("width must be >= 0")
 
         self.__width = value
 
