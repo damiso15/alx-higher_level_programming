34d7f327fc658d0719529e59f45a2a64716327d1
damiso <damiso15@yahoo.com>
2023-04-11 14:50:42 -0700

    3. Same class or inherit from

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,4 +1,4 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
 """
 A module that returns True if the object is an instance of, or if the
 object is an instance of a class that inherited from, the specified class;
