47c4e8159b7fdc35bdc54459c9558252e3a194a0
damiso <damiso15@yahoo.com>
2023-04-11 15:02:11 -0700

    4. Only sub class of

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -11,4 +11,4 @@ def inherits_from(obj, a_class):
     (directly or indirectly) from the specified class; otherwise False
     """
 
-    return(obj, a_class) and type(obj) != a_class
+    return isinstance(obj, a_class) and type(obj) != a_class
