c01b548ab72f97bf37ff3efa85e82c9970389878
damiso <damiso15@yahoo.com>
2023-04-11 15:46:35 -0700

    7. Integer validator

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+"""
+A class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    A class BaseGeometry
+    """
+
+    def area(self):
+        """
+        A Public Instance that raises an Exception with the message area() is
+        not implemented
+        """
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        A Public Instance that validates value
+        """
+
+        if type(value) != int:
+            raise TypeError(f"{name} must be an integer")
+
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
