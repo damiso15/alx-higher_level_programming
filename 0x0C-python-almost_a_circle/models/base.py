3acd3e3966ae22cd9e9bc1d7652c13c575164fd3
damiso <damiso15@yahoo.com>
2023-04-17 02:03:38 -0700

    1. Base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+A first class Base
+"""
+
+
+class Base:
+    """
+    A first class Base
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        A class constructor
+
+        Attributes:
+            id (int): the id
+        """
+
+        if id != None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
