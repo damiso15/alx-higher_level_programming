efbe541b79f57806292a0a855c5af7d82cf18b86
damiso <damiso15@yahoo.com>
2023-05-16 11:51:01 -0700

    17. Go to UTF8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -5,4 +5,6 @@
 -- Table first_table
 -- Field name in first_table
 USE hbtn_0c_0;
+ALTER DATABASE hbtn_0c_0 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table MODIFY COLUMN name VARCHAR(256);
