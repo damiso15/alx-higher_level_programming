dd7ecab72da80be366096169f656e13a95678000
damiso <damiso15@yahoo.com>
2023-05-16 12:13:08 -0700

    20. Temperatures #2

---
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -0,0 +1,8 @@
+-- Import in hbtn_0c_0 database this table dump
+-- script that displays the max temperature of each
+-- state (ordered by State name)
+SELECT STATE, MAX(value) AS max_temp
+FROM temperatures
+GROUP BY STATE
+ORDER BY STATE ASC
+LIMIT 3;
