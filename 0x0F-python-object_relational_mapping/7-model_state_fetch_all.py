54d9110144c37fa67b2804ca699de8c11828b277
damiso15 <damiso15@yahoo.com>
2023-06-16 21:08:50 +0100

    7. All states via SQLAlchemy

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+A script that lists all State objects from the database hbtn_0e_6_usa
+"""
+import sys
+#import logging
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model_state import Base, State
+
+
+if len(sys.argv) < 4:
+    print("Usage: python script.py username password database")
+    sys.exit(1)
+        
+username = sys.argv[1]
+password = sys.argv[2]
+database = sys.argv[3]
+
+# Create Logs
+#logging.basicConfig()
+#logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
+
+# Create Engine and Session
+engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost/{database}', pool_pre_ping=True)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Retrieve State objects and sort by id
+states = session.query(State).order_by(State.id).all()
+
+# Display results
+for state in states:
+    print(f"{state.id}: {state.name}")
+
+session.close()
