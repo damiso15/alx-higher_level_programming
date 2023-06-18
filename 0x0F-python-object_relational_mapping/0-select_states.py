4705513d6a10a2f48d2dff9bfeb59db4a07d182f
damiso15 <damiso15@yahoo.com>
2023-06-18 10:34:36 +0100

    0. Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """
 A script that lists all states from the database hbtn_0e_0_usa
+Usage: ./0-select_states.py <mysql username>
+                            <mysql password>
+                            <database name >
 """
 import sys
 import MySQLdb
