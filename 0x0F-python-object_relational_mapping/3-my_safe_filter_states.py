56c62a4ca82cc000433680524c69b01d7abf401a
damiso15 <damiso15@yahoo.com>
2023-06-19 09:54:40 +0100

    3. SQL Injection...

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,30 +1,36 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
 A script that takes in an argument and displays all values in the states table
 of hbtn_0e_0_usa where name matches the argument
+Usage: ./2-my_filter_states.py <mysql username>
+                                    <mysql password>
+                                    <database name>
+                                    <state name searched>
 """
 import sys
 import MySQLdb
 
 
-# Establish a connection
-conn = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        password=sys.argv[2],
-        database=sys.argv[3],
-        charset="utf8"
-        )
+if __name__ == "__main__":
+    # Establish a connection
+    conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            password=sys.argv[2],
+            database=sys.argv[3],
+            charset="utf8"
+            )
 
-# Performing database operations
-if len(sys.argv) > 4:
-    filter_word = sys.argv[4]
-    cur = conn.cursor()
-    query = "SELECT * FROM states WHERE name LIKE %s ORDER by id ASC"
-    cur.execute(query, ('%' + filter_word + '%',))
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
-    cur.close()
-    conn.close()
+    # Performing database operations
+    if len(sys.argv) > 4:
+        filter_word = sys.argv[4]
+        cur = conn.cursor()
+        query = """SELECT * FROM states WHERE name = '{}' ORDER by id ASC"""
+        cur.execute(query.format(filter_word))
+        query_rows = cur.fetchall()
+        for row in query_rows:
+            if row[1] == filter_word:
+                print(row)
+        cur.close()
+        conn.close()
