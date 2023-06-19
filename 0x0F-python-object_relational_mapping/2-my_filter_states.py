1faa43f0945bb62fc39fc4837b17332f6c23961d
damiso15 <damiso15@yahoo.com>
2023-06-19 09:07:26 +0100

    2. Filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -3,11 +3,11 @@
 A script that takes in an argument and displays all values in the states table
 of hbtn_0e_0_usa where name matches the argument
 """
-import sys
-import MySQLdb
-
-
 if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+
     # Establish a connection
     conn = MySQLdb.connect(
             host="localhost",
