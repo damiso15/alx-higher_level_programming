5066bb17291dffa2f38719db72c0d6a31ea881e0
damiso15 <damiso15@yahoo.com>
2023-06-19 08:55:46 +0100

    1. Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -26,6 +26,7 @@ if __name__ == "__main__":
                     ORDER by id ASC""")
         query_rows = cur.fetchall()
         for row in query_rows:
-            print(row)
+            if row[1][0] == "N":
+                print(row)
         cur.close()
         conn.close()
