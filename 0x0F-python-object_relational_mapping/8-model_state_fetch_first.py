81142b07cf34420ea1ca892092c869d39b59bc91
damiso15 <damiso15@yahoo.com>
2023-06-19 12:19:13 +0100

    8. First state

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+A script that prints the first State object from the database hbtn_0e_6_usa
+Usage: ./8-model_state_fetch_first.py root root hbtn_0e_6_usa
+"""
+import sys
+# import logging
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: python script.py username password database")
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Create Logs
+    # logging.basicConfig()
+    # logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
+
+    # Create Engine and Session
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        username, password, database), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Retrieve the first states
+    state = session.query(State).order_by(State.id).first()
+
+    # Display results
+    if state:
+        print(f"{state.id}: {state.name}")
+
+    # Close the session
+    session.close()
