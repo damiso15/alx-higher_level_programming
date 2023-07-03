585eef80eb443e4ebc6b7a8927ad92247cab4fa0
damiso15 <damiso15@yahoo.com>
2023-07-03 13:25:20 +0100

    4. cURL headers

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# A script that takes in a URL and displays all HTTP methods the server will accept.
+# A script that takes in a URL and displays all HTTP methods the server will accept
 curl -s -H "X-School-User-Id: 98" "$1"
