3db93e561b88ce73e96e2e8d83fc9abc70dad566
damiso15 <damiso15@yahoo.com>
2023-07-06 13:04:19 +0100

    0. What's my status? #0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# A Python script that fetches https://alx-intranet.hbtn.io/status
+import urllib.request
+
+url = 'https://alx-intranet.hbtn.io/status'
+
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+    print("Body response:")
+    print(f"    - type: {type(body)}")
+    print(f"    - content: {body}")
+    print(f"    - utf8 content: {body.decode('utf-8')}")
