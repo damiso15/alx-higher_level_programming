0e9479fb22a924f3dcfe5df6455360d9c811c94f
damiso15 <damiso15@yahoo.com>
2023-07-06 14:20:40 +0100

    2. POST an email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -17,4 +17,4 @@ if __name__ == "__main__":
 
     with urllib.request.urlopen(url, data) as response:
         body = response.read().decode('utf-8')
-        print("Your email is:", body)
+        print(body)
