c8d0612141a1ee8983ba313bf162c9af8715df01
damiso15 <damiso15@yahoo.com>
2023-07-06 13:31:29 +0100

    2. POST an email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,11 +8,13 @@ import sys
 import urllib.request
 import urllib.parse
 
-url = sys.argv[1]
-email = sys.argv[2]
 
-data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
 
-with urllib.request.urlopen(url, data) as response:
-    body = response.read().decode('utf-8')
-    print("Your email is:", body)
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+
+    with urllib.request.urlopen(url, data) as response:
+        body = response.read().decode('utf-8')
+        print("Your email is:", body)
