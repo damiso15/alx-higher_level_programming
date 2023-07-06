75ff850e429539c76fa422290cac3a3058e5018f
damiso15 <damiso15@yahoo.com>
2023-07-06 17:18:45 +0100

    6. POST an email #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,12 +7,14 @@ of the response
 import requests
 import sys
 
-url = sys.argv[1]
-email = sys.argv[2]
 
-data = {"email": email}
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
 
-response = requests.get(url, data=data)
-body = response.text
+    data = {"email": email}
 
-print(body)
+    response = requests.get(url, data=data)
+    body = response.text
+
+    print(body)
